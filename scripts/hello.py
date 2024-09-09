132bef6e10aa4f45a0768a2d085a1a1710f28e2a
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-09-09 14:58:35 -0300

    config for translate language

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,13 @@
+""" Script que imprime a data e o autor do último commit feito no repositório. """
+
 #!/usr/bin/env python3
+import gettext
 from dev_aberto import hello
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    gettext.bindtextdomain("cli", "locale")
+    gettext.textdomain("cli")
+    _ = gettext.gettext
+
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_("Último commit feito em: {} por {}").format(date, name))
